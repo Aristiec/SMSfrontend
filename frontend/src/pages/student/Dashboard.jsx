7d254b479f9e8774b5fa76f7d0063abe597e22bf
Harwinderiam <harwindercode@gmail.com>
2025-06-07 14:50:52 +0530

    Updated Student Dashboard By adding Calender and ExamTable component

--- frontend/src/pages/student/Dashboard.jsx
+++ frontend/src/pages/student/Dashboard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import WelcomeBanner from "../../components/student/dashboard/WelcomeBanner";
 import StatsCard from "../../components/student/dashboard/StatsCard";
-
+import ExamsTable from "../../components/student/dashboard/ExamsTable";
+import Calender from "../../components/student/dashboard/Calender";
 const Dashboard = () => {
   return (
-    <main className="p-6 bg-[#f5f7fa] flex flex-col gap-8 min-h-screen">
+    <main className="p-6 bg-[#E9EEF4] flex flex-col gap-8 min-h-screen">
       <WelcomeBanner />
       <StatsCard />
+      <div className="flex flex-row gap-6 ml-6">
+        <div className="flex-1">
+          <ExamsTable />
+        </div>
+        <div className="flex-1">
+          <Calender />
+        </div>
+      </div>
     </main>
   );
 };
