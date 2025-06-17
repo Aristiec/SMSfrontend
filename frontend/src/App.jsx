89af3d5851ed48d1331ccec9a2bf99f4fe6164fa
Harwinderiam <harwindercode@gmail.com>
2025-06-17 10:59:44 +0530

    Added faculty module structure and updated student layout and assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 
-import Header from "./components/student/Header";
-import Navbar from "./components/student/Navbar";
+import StudentRoutes from "./routes/StudentRoutes";
+import FacultyRoutes from "./routes/FacultyRoutes";
 
-// Import all student pages
-import StudentDashboard from "./pages/student/Dashboard";
-import Timetable from "./pages/student/Timetable";
-import Courses from "./pages/student/Courses";
-import Notices from "./pages/student/Notices";
-import Assignments from "./pages/student/Assignments";
-import Results from "./pages/student/Results";
-import Attendance from "./pages/student/Attendance";
-import FeePayment from "./pages/student/FeePayment";
-import Profile from "./pages/student/Profile";
-import AssignmentDetails from "./pages/student/AssignmentDetails";
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-[#E9EEF4] flex pt-[72px]">
-        {/* Fixed Header */}
-        <Header />
-
-        {/* Fixed Navbar */}
-        <nav className="w-[240px] fixed left-0 top-[72px] bottom-0 bg-white shadow-md overflow-y-auto scrollbar-hide">
-          <Navbar />
-        </nav>
-
-        {/* Content */}
-        <div className="flex-1 ml-[240px] p-4">
-          <Routes>
-            <Route path="/" element={<StudentDashboard />} />
-            <Route path="/dashboard" element={<StudentDashboard />} />
-            <Route path="/timetable" element={<Timetable />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/notices" element={<Notices />} />
-            <Route path="/assignments" element={<Assignments />} />
-            <Route path="/assignments/:id" element={<AssignmentDetails />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="/attendance" element={<Attendance />} />
-            <Route path="/fees" element={<FeePayment />} />
-            <Route path="/profile" element={<Profile />} />
-            {/* Optional default redirect or 404 */}
-            <Route path="*" element={<StudentDashboard />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        {StudentRoutes()}
+        {FacultyRoutes()}
+      </Routes>
     </Router>
   );
 }
