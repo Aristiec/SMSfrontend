931ad0badd14e08654d761b0ab7c52a94a72190c
surajsibi <surajsibi2022@gmail.com>
2025-06-24 17:15:58 +0530

    general assistant

--- frontend/src/layouts/StudentLayout.jsx
+++ frontend/src/layouts/StudentLayout.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/student/Header";
 import Navbar from "../components/student/Navbar";
 import { Outlet } from "react-router-dom";
 import GeneralAssistant from "../components/student/Ai/generalAssistant";
+// import { logo } from "../assets/logo.svg";
 
 const StudentLayout = () => {
+  const [showGeneralAssistant, setShowGeneralAssistant] = useState(true);
   return (
     <div className="min-h-screen bg-[#E9EEF4] flex pt-[72px] relative">
       <Header />
@@ -14,11 +16,29 @@ const StudentLayout = () => {
       <div className="flex-1 ml-[240px] p-4">
         <Outlet />
       </div>
-      <div className="fixed right-0 top-0 z-500 bg-[#FAFCFD] w-[36%] h-[100dvh]">
-        <GeneralAssistant/>
+      <div className="">
+        <div
+          style={{
+            background:
+              "linear-gradient(311.58deg, #04203E 14.78%, #0077FF 98.82%)",
+          }}
+          className=""
+        >
+          {/* <img src={logo}/> */}
+        </div>
       </div>
+
+      {showGeneralAssistant && (
+        <div>
+          <div className="absolute inset-0 bg-[#1F1D1D]/[0.24] z-10"></div>
+
+          <div className="fixed right-0 top-0 z-500 bg-[#FAFCFD] w-[30%] ">
+            <GeneralAssistant />
+          </div>
+        </div>
+      )}
     </div>
   );
-};  
+};
 
 export default StudentLayout;
