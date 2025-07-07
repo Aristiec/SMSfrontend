79041cc6e0f42f1cb2e90a6a1363be1caa5c507c
surajsibi <surajsibi2022@gmail.com>
2025-07-07 19:40:39 +0530

    table fix

--- frontend/src/components/student/hostel/LowerSection.jsx
+++ frontend/src/components/student/hostel/LowerSection.jsx
@@ -3,6 +3,7 @@ import { House, CircleAlert, Wrench, Book, Phone } from "lucide-react";
 import Maintenance from "./Maintenance";
 import HostelAndRooms from "./HostelAndRooms";
 import RulesAndTiming from "./RulesAndTiming";
+import { motion, AnimatePresence } from "framer-motion";
 import PaymentHistory from "./PaymentHistory";
 const menuItems = [
   {
@@ -50,17 +51,25 @@ const LowerSection = () => {
           </button>
         ))}
       </div>
-      <div>
-        {selectedIndex === 0 ? (
-          <HostelAndRooms />
-        ) : selectedIndex === 1 ? (
-          <RulesAndTiming />
-        ) : selectedIndex === 2 ? (
-          <Maintenance />
-        ) : selectedIndex === 3 ? (
-          <PaymentHistory />
-        ) : null}
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+        animate={{opacity:1, y:0}}
+          initial={{ opacity: 0, y: 10 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+          key={selectedIndex}
+        >
+          {selectedIndex === 0 ? (
+            <HostelAndRooms />
+          ) : selectedIndex === 1 ? (
+            <RulesAndTiming />
+          ) : selectedIndex === 2 ? (
+            <Maintenance />
+          ) : selectedIndex === 3 ? (
+            <PaymentHistory />
+          ) : null}
+        </motion.div>
+      </AnimatePresence>
     </section>
   );
 };
