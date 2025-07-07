242115624dcb05921e374a535e257f1563cecc31
surajsibi <surajsibi2022@gmail.com>
2025-07-07 15:35:52 +0530

    hostel

--- frontend/src/pages/student/Hostel.jsx
+++ frontend/src/pages/student/Hostel.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Header from "../../components/student/hostel/Header";
+import LowerSection from "../../components/student/hostel/LowerSection.jsx";
 const Hostel = () => {
-  return <div>Hostel</div>;
+  return (
+    <div className="flex flex-col px-4 gap-7 mt-4  ">
+      <Header />
+      <LowerSection/>
+    </div>
+  );
 };
 
 export default Hostel;
