fb08c296a493c6d8e3fcc6dd46f9b8ed76919dfa
Harwinderiam <harwindercode@gmail.com>
2025-07-07 16:55:52 +0530

    Updated courses with backend

--- frontend/src/features/auth/authAPI.js
+++ frontend/src/features/auth/authAPI.js
@@ -4,3 +4,6 @@ export const loginUserAPI = (credentials) =>
   api.post("/auth/login", credentials);
 export const fetchProfileByEmail = (email) =>
   api.get(`/student/email/${email}`);
+
+export const fetchSubjectsByCourseId = (courseId) =>
+  api.get(`/subjects/course/${courseId}/subjects`);
