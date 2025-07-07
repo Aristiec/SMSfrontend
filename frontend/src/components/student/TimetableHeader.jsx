d5a6ed648d285fdd576dc0637ad3c908f9570e61
surajsibi <surajsibi2022@gmail.com>
2025-07-07 22:17:43 +0530

    header fixed

--- frontend/src/components/student/TimetableHeader.jsx
+++ frontend/src/components/student/TimetableHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TimetableHeader = () => {
   return (
-    <div className="flex justify-center px-4 lg:px-4 mt-4 sticky top-20 z-1000">
+    <div className="flex justify-center px-4 lg:px-4 mt-4 sticky top-20 z-100">
       <header className="bg-[#04203e] flex flex-col sm:flex-row justify-between items-start sm:items-center rounded-lg w-full max-w-7xl px-6 py-4 text-[#FAFCFD] font-[Inter]">
         <h1 className="text-[20px] sm:text-[24px] font-bold font-[Merriweather] mb-2 sm:mb-0">
           Schedule
