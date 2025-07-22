55b4e80e2597d4f11e340b8541db5851f8c6e48a
Harwinderiam <harwindercode@gmail.com>
2025-07-22 15:34:06 +0530

    Updated backend api

--- frontend/src/pages/student/Assignments.jsx
+++ frontend/src/pages/student/Assignments.jsx
@@ -100,7 +100,7 @@ const Assignments = () => {
   const handleMarkAsCompleted = async (assignment) => {
     try {
       await fetch(
-        "https://ebaf0b43591f.ngrok-free.app/api/v1/submission/submit-offline",
+        "https://39bd5ed85d57.ngrok-free.app/api/v1/submission/submit-offline",
         {
           method: "POST",
           headers: {
