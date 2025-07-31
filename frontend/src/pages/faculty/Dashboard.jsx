2e5274a81207d81965f44e5b6b3c68ea8d1fc7fc
CR <vu4f2223004@pvppcoe.ac.in>
2025-07-31 15:16:38 +0530

    changes

--- frontend/src/pages/faculty/Dashboard.jsx
+++ frontend/src/pages/faculty/Dashboard.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import OfflineExam from "../../components/faculty/OfflineExam";
-import AttendenceRecords from "../../components/faculty/AttendenceRecords";
-// import SubjectiveQuestion from "../../components/faculty/SubjectiveQuestion";
+
 
 const Dashboard = () => {
   return <>
-  <OfflineExam/>
-  <AttendenceRecords/>
+Dashboard
+
   </>;
 };
 
